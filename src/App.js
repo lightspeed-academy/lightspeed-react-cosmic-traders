e0fecdc9732806ab4e85b209cf39a3dedfad85e1
Jim Cooper <jim@cooperplanet.com>
2022-07-30 11:00:15 -0600

    Add missing redirect route

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  Navigate
+  Navigate,
 } from 'react-router-dom'
 
 import Outpost from './outpost/Outpost'
 import Checkout from './checkout/Checkout'
+import Header from './header/Header'
 import styles from './App.module.css'
 
 function App() {
   return (
-    <div className={styles.content}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <div className={styles.content}>
         <Routes>
           <Route path="/outpost" element={<Outpost />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/" element={<Navigate to="/outpost" />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
