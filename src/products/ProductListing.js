f1fea01e66561a07410d5def2652690bc345aed2
Jim Cooper <jim@cooperplanet.com>
2022-08-01 11:39:36 -0600

    18-AccessingQueryParameters Complete

--- src/products/ProductListing.js
+++ src/products/ProductListing.js
@@ -1,16 +1,36 @@
+import { useSearchParams } from 'react-router-dom'
+
 import products from '../products/products.json'
 import styles from './ProductListing.module.scss'
 import Product from './Product'
 
 function ProductListing(props) {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const filter = searchParams.get('filter')
+
+  function setFilter(filter) {
+    setSearchParams({ filter })
+  }
+
   return (
-    <ul className={styles.products}>
-      {products.map((product, idx) => (
-        <li key={idx} >
-          <Product productData={product} addToCart={props.addToCart} />
-        </li>)
-      )}
-    </ul>
+    <>
+      <div className={styles.filters}>
+        <button className="button" onClick={() => setFilter('transport')}>Transport</button>
+        <button className="button" onClick={() => setFilter('weapons')}>Weapons</button>
+        <button className="button" onClick={() => setFilter('defense')}>Defense</button>
+        <button className="button" onClick={() => setFilter('equipment')}>Equipment</button>
+        <button className="button" onClick={() => setFilter('')}>All</button>
+      </div>
+      <ul className={styles.products}>
+        {products
+          .filter(p => !filter || p.category === filter)
+          .map((product, idx) => (
+            <li key={idx} >
+              <Product productData={product} addToCart={props.addToCart} />
+            </li>)
+          )}
+      </ul>
+    </>
   )
 }
 export default ProductListing
